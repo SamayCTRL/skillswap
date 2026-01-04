81d0b92427b00e8d7ec053a4318efd43f8525da1
Skill Swap Developer <developer@skillswap.local>
2026-01-04 12:07:38 +0400

    Update catch-all route to handle all non-API requests

--- server/server.js
+++ server/server.js
@@ -208,7 +208,7 @@ app.use('/api/*', (req, res) => {
 });
 
 // Serve frontend for non-API routes
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
